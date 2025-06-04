29826a34581cdd6ecb2944b42c4c1fe91a8f99f5
Cotonho <marcoantoniomilagre587@gmail.com>
2025-06-04 18:59:53 -0300

    pequenas alterações

--- teste.py
+++ teste.py
@@ -2,6 +2,8 @@
 import re
 invalido = 0
 contadorOperadores = 0
+Os_Necessarios = 0
+Es_Necessarios = 0
 expressao = input("Digite uma expressão: ")
 operadores = "()+-*/"
 
@@ -9,18 +11,30 @@ operadores = "()+-*/"
 partes = re.split(r'([(+\-*/)])', expressao)
 
 
-print(f"{partes}")
+print(partes)
+
+partes = list(filter(None, partes))
 
 try: # esse código ainda passa as verificações em operações como 1++
-    for atual, prox, prox_prox in zip(partes, partes[1:], partes[2:]):
+    for atual in partes:
+
         if atual in operadores:
-            continue
+            if atual == '(':
+                Es_Necessarios += 1
+            elif atual == ')':
+                continue
+            else: 
+                Os_Necessarios += 1
+            
         else:
+            Es_Necessarios += 1
             int(atual)
 
     # verifica se os valores são válidos
 except TypeError: invalido = 1, print(f"Valor inválido na posição .")
 except ValueError: invalido = 1, print(f"Valor inválido na posição .")
+
+print(Os_Necessarios, Es_Necessarios)
 
 if invalido: print("Portanto essa expressão não é válida.")
 else:print('passou')
